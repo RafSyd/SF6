96e8403041e70f903ef1f8119b708e23d8d7ab76
RafSyd <161016502+RafSyd@users.noreply.github.com>
2024-03-07 09:07:37 +1100

    Update viewMoreLess.js

--- viewMoreLess.js
+++ viewMoreLess.js
@@ -3,22 +3,26 @@ document.addEventListener('DOMContentLoaded', function () {
     const syncTrigger = document.getElementById('sync-trigger');
     const submenusync = syncSection.querySelector('.fly');
     const toggleIconsync = syncSection.querySelector('.toggle');
+    const additionalInfo = syncSection.querySelector('.additional-info');
 
     // Initially hide the submenu
     submenusync.style.display = 'none';
 
     // Get the initial text from line 5
     const initialText = syncTrigger.querySelector('.toggle').textContent.trim();
-    toggleIconsync.innerHTML = initialText + ' <img src="https://d2oir2m0sodeh3.cloudfront.net/products/VIEWmORE.png" alt="Icon" width="35" height="25">';
+    toggleIconsync.innerHTML = '<span style="color: red; font-weight: bold;">+</span> ' + initialText;
+    additionalInfo.style.display = 'inline'; // Display additional info by default
 
     syncTrigger.addEventListener('click', function (event) {
         event.preventDefault();
         if (submenusync.style.display === 'block') {
             submenusync.style.display = 'none';
-            toggleIconsync.innerHTML = initialText + ' <img src="https://d2oir2m0sodeh3.cloudfront.net/products/VIEWmORE.png" alt="Icon" width="35" height="25">';
+            toggleIconsync.innerHTML = '<span style="color: red; font-weight: bold;">+</span> ' + initialText;
+            additionalInfo.style.display = 'inline'; // Show additional info when menu is closed
         } else {
             submenusync.style.display = 'block';
-            toggleIconsync.innerHTML = initialText + ' <img src="https://d2oir2m0sodeh3.cloudfront.net/products/VIEWLESS.png" alt="Icon" width="35" height="25">';
+            toggleIconsync.innerHTML = '<span style="color: red; font-weight: bold;">-</span> ' + initialText;
+            additionalInfo.style.display = 'none'; // Hide additional info when menu is open
         }
     });
 
